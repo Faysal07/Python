592ac1a32f088222fd99092b6d3b528916f5486c
Sheikh Faysal Tareque <43994860+Faysal07@users.noreply.github.com>
2021-05-01 23:06:37 +0600

    Add files via upload

---
+++ listProblem.py
@@ -0,0 +1,11 @@
+#Problem 1 Solve
+list1 = [100,300,200,400,600,500]
+newList = list1[5:1:-1]
+print(newList)
+
+#Problem 2 Solve
+list2 = ["Fay","Nu","Meh"]
+list3 = ["sal","man","edi"]
+
+list4 = [i+j for i, j in zip(list2,list3)]
+print(list4)
